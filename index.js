5858ab072840f226b1e27d1c56d4853242f7cee3
unknown <nenegrakrina@gmail.com>
2021-04-25 15:51:22 +0300

    View class realization

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 import {Game} from './src/Game.js';
+import {View} from './src/View.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
 window.game = game;
+window.view = view;
+
+document.addEventListener('keydown', event => {
+  switch (event.keyCode) {
+    case 37:
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    case 38:
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+    case 39:
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    case 40:
+      game.movePieceDown();
+      view.render(game.getState());
+      break;
+  }
+});
